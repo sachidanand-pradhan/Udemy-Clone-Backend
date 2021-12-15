c245d7908cebc7e34b1f7412395beb1e5b3157a7
Sachidanand Pradhan <sachidanand.kpradhan@gmail.com>
2021-12-15 19:24:42 +0530

    commit

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("", async function (req, res) {
     // console.log("Yes")
     try {
-        console.log("---Yes")
+        // console.log("---Yes")
         const course = await Course.create(req.body);
         return res.status(200).send(course);
     }
@@ -28,18 +28,18 @@ router.get("", async (req, res) => {
         return res.status(500).json({ message: e.message, status: "Failed" })
     }
 })
-// router.get("/search", async (req, res) => {
-//     try {
+router.get("/search", async (req, res) => {
+    try {
         
-//         const course = await Course.find().lean().exec()
-//         return res.render("home", {
-//             course,
-//         });
+        const course = await Course.find().lean().exec()
+        return res.render("courses", {
+            course,
+        });
         
-//     } catch (e) {
-//         return res.status(500).json({ message: e.message, status: "Failed" })
-//     }
-// })
+    } catch (e) {
+        return res.status(500).json({ message: e.message, status: "Failed" })
+    }
+})
 
 router.get("/show", async function (req, res) {
     try {
@@ -48,36 +48,6 @@ router.get("/show", async function (req, res) {
     }catch (e) {
         return res.status(500).json({ message: e.message, status: "Failed" })
     }
-})
-
-// router.get("/:id", async (req, res) => {
-//     try {
-//         const author = await Course.findById(req.params.id).lean().exec()
-//         return res.status(201).send(author)
-//     } catch (e) {
-//         return res.status(500).json({ message: e.message, status: "Failed" })
-//     }
-// })
-
-// router.patch("/:id", async (req, res) => {
-//     try {
-//         const author = await Course.findByIdAndUpdate(req.params.id, req.body, {
-//             new: true
-//         }).lean().exec()
-//         res.status(201).send(author)
-//     } catch (e) {
-//         return res.status(500).json({ message: e.message, status: "Failed" })
-//     }
-// })
-
-// router.delete("/:id", async (req, res) => {
-//     try {
-//         const author = await Author.findByIdAndDelete(req.params.id).lean().exec()
-//         res.status(201).send(author)
-//     } catch (e) {
-//         return res.status(500).json({ message: e.message, status: "Failed" })
-//     }
-// })
-
+});
 
 module.exports = router;
