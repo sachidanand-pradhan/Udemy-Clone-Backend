b58efc2ea28876ec442ca5d1f261dceea5fafa93
Sachidanand Pradhan <sachidanand.kpradhan@gmail.com>
2021-12-15 16:56:55 +0530

    commit

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -1,68 +1,71 @@
 const express = require('express');
-const Author = require('../models/course.model');
+const Course = require('../models/course.model');
 
 const router = express.Router();
 
-router.post('', async function (req, res) {
+router.post("", async function (req, res) {
+    // console.log("Yes")
     try {
-        const author = await Author.create(req.body);
-        return res.status(200).send(author);
+        console.log("---Yes")
+        const course = await Course.create(req.body);
+        return res.status(200).send(course);
     }
     catch (e) {
-        return res.status(500).json({message: e.message, status: "Failed"})
+        return res.status(500).json({ message: e.message, status: "Failed" })
     }
 });
 
-// router.get("", async function (req, res) {
-//     const products = await Product.find().lean().exec();
-//     console.log(products);
-
-//     return res.render("products/all",{
-//         products,
-//     });
-// })
 
 router.get("", async (req, res) => {
-    try{
-
-        const author = await Author.find().lean().exec()
-        return res.render("home",{
-            author,
+    try {
+        
+        const course = await Course.find().lean().exec()
+        return res.render("home", {
+            course,
         });
-
-    }catch(e){
-        return res.status(500).json({message: e.message, status: "Failed"})
+        
+    } catch (e) {
+        return res.status(500).json({ message: e.message, status: "Failed" })
     }
 })
 
-router.get("/:id", async (req, res) => {
-    try{
-    const author = await Author.findById(req.params.id).lean().exec()
-    return res.status(201).send(author)
-    }catch(e){
-        return res.status(500).json({message: e.message, status: "Failed"})
+router.get("/show", async function (req, res) {
+    try {
+        const course = await Course.find().lean().exec();
+        res.status(201).send({course});
+    }catch (e) {
+        return res.status(500).json({ message: e.message, status: "Failed" })
     }
 })
 
-router.patch("/:id", async (req, res) => {
-    try{
-        const author = await Author.findByIdAndUpdate(req.params.id, req.body, {
-            new: true
-        }).lean().exec()
-        res.status(201).send(author)
-    }catch(e){
-        return res.status(500).json({message: e.message, status: "Failed"})
-    }
-})
+// router.get("/:id", async (req, res) => {
+//     try {
+//         const author = await Course.findById(req.params.id).lean().exec()
+//         return res.status(201).send(author)
+//     } catch (e) {
+//         return res.status(500).json({ message: e.message, status: "Failed" })
+//     }
+// })
 
-router.delete("/:id", async (req, res) => {
-    try{
-        const author = await Author.findByIdAndDelete(req.params.id).lean().exec()
-        res.status(201).send(author)
-    }catch(e){
-        return res.status(500).json({message: e.message, status: "Failed"})
-    }
-})
+// router.patch("/:id", async (req, res) => {
+//     try {
+//         const author = await Course.findByIdAndUpdate(req.params.id, req.body, {
+//             new: true
+//         }).lean().exec()
+//         res.status(201).send(author)
+//     } catch (e) {
+//         return res.status(500).json({ message: e.message, status: "Failed" })
+//     }
+// })
+
+// router.delete("/:id", async (req, res) => {
+//     try {
+//         const author = await Author.findByIdAndDelete(req.params.id).lean().exec()
+//         res.status(201).send(author)
+//     } catch (e) {
+//         return res.status(500).json({ message: e.message, status: "Failed" })
+//     }
+// })
 
 
 module.exports = router;
