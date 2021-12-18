e6e2e607ac42ba35c590782efc189ceb7a4c3016
Suraj Karosia <53578095+1998Suraj@users.noreply.github.com>
2021-12-18 22:59:05 +0530

    commit

--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -1,57 +1,69 @@
-const express = require('express');
-const signup = require('../models/signup.model');
+const express = require("express");
+const signup = require("../models/signup.model");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const cookie = require('cookie');
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
 
-const Author = require('../models/course.model')
+const Author = require("../models/course.model");
 
 router.get("", async (req, res) => {
-    try {
-        const author = Author.find().lean().exec();
-        const cookie = req.cookies.jwt;
-        return res.render("signup", { author, cookie });
-    } catch (e) {
-        return res.status(500).json({ message: e.message, status: "Failed" })
-    }
+  try {
+    const author = Author.find().lean().exec();
+    const cookie = req.cookies.jwt;
+    return res.render("signup", { author, cookie });
+  } catch (e) {
+    return res.status(500).json({ message: e.message, status: "Failed" });
+  }
 });
 
 router.post("", async (req, res) => {
-    try {
-        let token;
-       const registerUser = new signup({
-        full_Name : req.body.fullName,
-        email : req.body.Email,
-        password : req.body.Password
-       });
+  try {
+    let token;
+    const registerUser = new signup({
+      full_Name: req.body.fullName,
+      email: req.body.Email,
+      password: req.body.Password,
+    });
 
-       token = await registerUser.generateAuthToken();
-       console.log("Token is being generated");
-       
-       res.cookie("jwt", token,{
-           expires:new Date(Date.now() + 6000000000000000),
-           httpOnly:true
-       });
-       console.log(cookie);
+    const checkUser = await signup.findOne({ email: email }).lean().exec();
+    if (checkUser) {
+        console.log("You are already a user");
+      alert("You are already a user");
+    } else {
+      token = await registerUser.generateAuthToken();
+      console.log("Token is being generated");
+
+      res.cookie("jwt", token, {
+        expires: new Date(Date.now() + 6000000000000000000),
+        httpOnly: true,
+      });
+      console.log(cookie);
 
       const register = await registerUser.save();
       console.log(register);
 
       return res.redirect("/home");
-
-    } catch (e) {
-        return res.status(500).json({ message: e.message, status: "error is in this particular block" })
     }
+  } catch (e) {
+    return res
+      .status(500)
+      .json({
+        message: e.message,
+        status: "error is in this particular block",
+      });
+  }
 });
 
 router.get("/:token", async (req, res) => {
-    try {
-        const user = await signup.find({tokens: {$elemMatch: {token: req.params.token}}}).lean().exec();
-        res.status(200).send(user);
-
-    } catch (e) {
-        return res.status(500).json({ message: e.message, status: "Failed" })
-    }
+  try {
+    const user = await signup
+      .find({ tokens: { $elemMatch: { token: req.params.token } } })
+      .lean()
+      .exec();
+    res.status(200).send(user);
+  } catch (e) {
+    return res.status(500).json({ message: e.message, status: "Failed" });
+  }
 });
 
 module.exports = router;
