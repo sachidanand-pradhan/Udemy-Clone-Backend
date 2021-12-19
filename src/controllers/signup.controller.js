108bee4eeb30245ce27fc7bff81b8c9e22514769
Suraj Karosia <53578095+1998Suraj@users.noreply.github.com>
2021-12-19 15:06:51 +0530

    commit

--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -20,24 +20,31 @@ router.post("", async (req, res) => {
   try {
     let token;
     const registerUser = new signup({
-      full_Name: req.body.fullName,
-      email: req.body.Email,
-      password: req.body.Password,
+      full_Name:req.body.fullName,
+      email:req.body.Email,
+      password:req.body.Password,
     });
 
-    token = await registerUser.generateAuthToken();
-    console.log("Token is being generated");
+    console.log("signup working")
+    // const checkUser = await signup.findOne({ email: email }).lean().exec();
+    // if (checkUser) {
+    //     console.log("You are already a user");
+    //   alert("You are already a user");
+    // } else {
+      token = await registerUser.generateAuthToken();
+      console.log("Token is being generated");
 
-    res.cookie("jwt", token, {
-      expires: new Date(Date.now() + 6000000000000000000),
-      httpOnly: true,
-    });
-    console.log(cookie);
+      res.cookie("jwt", token, {
+        expires: new Date(Date.now() + 6000000000000000000),
+        httpOnly: true,
+      });
+     console.log(cookie);
 
     const register = await registerUser.save();
     console.log(register);
 
-    return res.redirect("/home");
+      return res.redirect("/home");
+   // }
   } catch (e) {
     return res.status(500).json({
       message: e.message,
