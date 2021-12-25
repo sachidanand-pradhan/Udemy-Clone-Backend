3abed3c3c2a5d4bffde376e83d296e61320d6336
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2021-12-25 20:17:13 +0530

    Update cart.controller.js

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,17 +1,24 @@
 const express = require('express');
+
 const router = express.Router();
+
 const Author = require('../models/course.model');
 
 
 
 router.get("/", async function (req, res) {
     try {
+
         const author = await Author.find().lean().exec();
+
         const cookie = req.cookies.jwt;
+
         return res.render("cart", { author, cookie });
 
     } catch (e) {
+
         return res.status(500).json({ message: e.message, status: "failed at cart.controller" })
+        
     }
 })
 
