95eafa605b93657c2cf50e44ff7b7b840219f34d
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-01-11 09:55:42 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
     return mongoose.connect("mongodb+srv://sachida44:sachida44@cluster0.xiqh3.mongodb.net/udemy");
-    // return mongoose.connect("mongodb://127.0.0.1:27017/movies");
+    
 
     // This is for me for testing purpose... Please don't remove it.... BIGDADDY WALA THANKYOU
     //return mongoose.connect("mongodb://127.0.0.1:27017/udemy");
